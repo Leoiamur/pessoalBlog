96c8ec6f8ef2d71100cd5c28f50761b9d5857edd
Leoiamur <leoiamur@hotmail.com>
2020-12-03 17:19:02 -0300

    Introdução ao consumo de API e Segurança

--- pessoalBlog/src/app/perfil-lateral/perfil-lateral.component.ts
+++ pessoalBlog/src/app/perfil-lateral/perfil-lateral.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil-lateral.component.css']
 })
 export class PerfilLateralComponent implements OnInit {
+  
+  nome: string = 'Leonardo'
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
 }
